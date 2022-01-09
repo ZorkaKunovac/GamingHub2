16416210e99cc7f53f65adf4847d7b1c5966da71
Zorka Kunovac <61622729+ZorkaKunovac@users.noreply.github.com>
2022-01-09 21:29:58 +0100

    Test AddingNewKupac

--- GamingHub2/Services/KupacService.cs
+++ GamingHub2/Services/KupacService.cs
@@ -3,6 +3,7 @@ using GamingHub2.Model.Requests;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace GamingHub2.Services
@@ -17,7 +18,7 @@ namespace GamingHub2.Services
         public override IEnumerable<Model.Kupac> Get(KupacSearchRequest search)
         {
             var entity = Context.Set<Database.Kupac>().AsQueryable();
-            if(search.KorisnikId.HasValue)
+            if (search.KorisnikId.HasValue)
             {
                 entity = entity.Where(x => x.KorisnikId == search.KorisnikId.Value);
             }
@@ -25,5 +26,95 @@ namespace GamingHub2.Services
             var list = entity.ToList();
             return _mapper.Map<List<Model.Kupac>>(list);
         }
+
+        public override Model.Kupac Insert(KupacUpsertRequest request)
+        {
+            var entity = _mapper.Map<Database.Kupac>(request);
+
+            AddKupac(entity);
+
+            return _mapper.Map<Model.Kupac>(entity);
+        }
+
+        public void AddKupac(Database.Kupac entity)
+        {
+            if (entity.KorisnikId == 0)
+            {
+                throw new ArgumentException("Invalid parameter ", "KorisnikId");
+            }
+
+            if (string.IsNullOrWhiteSpace(entity.Ime))
+            {
+                throw new ArgumentException("Invalid parameter ", "Ime");
+            }
+            else
+            {
+                if (!Regex.IsMatch(entity.Ime, @"^[A-Z][A-Za-z \t-]{2,50}$"))
+                {
+                    throw new ArgumentException("Invalid format ", "Ime");
+                }
+            }
+
+            if (string.IsNullOrWhiteSpace(entity.Prezime))
+            {
+                throw new ArgumentException("Invalid parameter ", "Prezime");
+            }
+            else
+            {
+                if (!Regex.IsMatch(entity.Prezime, @"^[A-Z][A-Za-z \t-]{2,50}$"))
+                {
+                    throw new ArgumentException("Invalid format ", "Prezime");
+                }
+            }
+
+            if (string.IsNullOrWhiteSpace(entity.Email))
+            {
+                throw new ArgumentException("Invalid parameter ", "Email");
+            }
+            else
+            {
+                if (!Regex.IsMatch(entity.Email, @"^[a-z]+[-+.'\w]+@[a-z]+\.[-.\w]+$", RegexOptions.IgnoreCase))
+                {
+                    throw new ArgumentException("Invalid format ", "Email");
+                }
+            }
+
+            if (string.IsNullOrWhiteSpace(entity.BrojTelefona))
+            {
+                throw new ArgumentException("Invalid parameter ", "BrojTelefona");
+            }
+            else
+            {
+                if (!Regex.IsMatch(entity.BrojTelefona, @"^[+]?\d{3}[ ]?\d{2}[ ]?\d{3}[ ]?\d{3,4}$", RegexOptions.IgnoreCase))
+                    throw new ArgumentException("Invalid format ", "BrojTelefona");
+            }
+
+            if (string.IsNullOrWhiteSpace(entity.Adresa1))
+                throw new ArgumentException("Invalid parameter ", "Adresa1");
+            else if (entity.Adresa1.Length < 5 || entity.Adresa1.Length > 70)
+                throw new ArgumentException("Invalid format ", "Adresa1");
+
+            if (!string.IsNullOrEmpty(entity.Adresa2) && (entity.Adresa2.Length < 2 || entity.Adresa2.Length > 70))
+                throw new ArgumentException("Invalid format ", "Adresa2");
+
+            if (string.IsNullOrWhiteSpace(entity.Drzava))
+                throw new ArgumentException("Invalid parameter ", "Drzava");
+            else if (entity.Drzava.Length < 3 || entity.Drzava.Length > 20)
+                throw new ArgumentException("Invalid format ", "Drzava");
+
+            if (string.IsNullOrWhiteSpace(entity.Grad))
+                throw new ArgumentException("Invalid parameter ", "Grad");
+            else if (entity.Grad.Length < 5 || entity.Grad.Length > 70)
+                throw new ArgumentException("Invalid format ", "Grad");
+
+            if (string.IsNullOrWhiteSpace(entity.PostanskiBroj))
+                throw new ArgumentException("Invalid parameter ", "PostanskiBroj");
+            else if (entity.PostanskiBroj.Length < 3 || entity.PostanskiBroj.Length > 20)
+                throw new ArgumentException("Invalid format ", "PostanskiBroj");
+
+            Context.Set<Database.Kupac>().Add(entity);
+            Context.SaveChanges();
+        }
+
     }
 }
---
+++ GamingHub2UnitTest/TestAddingNewKupac.cs
@@ -0,0 +1,172 @@
+﻿using AutoMapper;
+using GamingHub2;
+using GamingHub2.Model.Requests;
+using GamingHub2.Services;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace GamingHub2UnitTest
+{
+    public class TestAddingNewKupac
+    {
+        public static ApplicationDbContext _context;
+        private static IMapper _mapper;
+
+        public TestAddingNewKupac()
+        {
+
+            if (_mapper == null)
+            {
+                var mappingConfig = new MapperConfiguration(mc =>
+                {
+                    mc.AddProfile(new GamingHub2.Mapping.GamingHubProfile());
+                });
+                IMapper mapper = mappingConfig.CreateMapper();
+                _mapper = mapper;
+            }
+        }
+
+        [Theory]
+        [InlineData("Zorka", "Kunovac", "zorka.kunovac1@hotmail.com", "+38761000111", "Sjeverni logor", "Sjeverni logor", "Mostar", "Bosna i Hercegovina", "88000",1)]
+        [InlineData("Zorka", "Kunovac", "zorka.kunovac1@hotmail.com", "+38761000111", "Sjeverni logor", "", "Mostar", "Bosna i Hercegovina", "88000", 1)]
+        public void AddKupac_EmptyField_ShouldWork(string ime, string prezime, string email, string brojTelefona, string adresa1, string adresa2, string grad, string drzava, string postanskiBroj , int korisnikId)
+        {
+            KupacUpsertRequest request = new KupacUpsertRequest()
+            {
+                Ime = ime,
+                Prezime = prezime,
+                Email = email,
+                BrojTelefona = brojTelefona,
+                Adresa1 = adresa1,
+                Adresa2 = adresa2,
+                Grad = grad,
+                Drzava = drzava,
+                PostanskiBroj = postanskiBroj,
+                KorisnikId = korisnikId
+            };
+
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase(databaseName: "KupacContext1")
+            .Options;
+
+            using (_context = new ApplicationDbContext(options))
+            {
+                KupacService _service = new KupacService(_context, _mapper);
+                //assert & act
+                var validKupac = _service.Insert(request);
+                Assert.NotNull(validKupac);
+            }
+        }
+
+        [Theory]
+        [InlineData("", "Kunovac", "zorka.kunovac1@hotmail.com", "+38761000111", "Sjeverni logor", "Sjeverni logor", "Mostar", "Bosna i Hercegovina", "88000", 1, "Ime")]
+        [InlineData("Zorka", "", "zorka.kunovac@hotmail.com", "+38761000111", "Sjeverni logor", "Sjeverni logor", "Mostar", "Bosna i Hercegovina", "88000", 1, "Prezime")]
+        [InlineData("Zorka", "Kunovac", "", "+38761000111", "Sjeverni logor", "Sjeverni logor", "Mostar", "Bosna i Hercegovina", "88000", 1, "Email")]
+        [InlineData("Zorka", "Kunovac", "zorka.kunovac1@hotmail.com", "", "Sjeverni logor", "Sjeverni logor", "Mostar", "Bosna i Hercegovina", "88000", 1, "BrojTelefona")]
+
+        [InlineData("Zorka", "Kunovac", "zorka.kunovac1@hotmail.com", "+38761000111", "", "Sjeverni logor", "Mostar", "Bosna i Hercegovina", "88000", 1, "Adresa1")]
+        [InlineData("Zorka", "Kunovac", "zorka.kunovac1@hotmail.com", "+38761000111", "Sjeverni logor", "Sjeverni logor", "", "Bosna i Hercegovina", "88000", 1, "Grad")]
+        [InlineData("Zorka", "Kunovac", "zorka.kunovac1@hotmail.com", "+38761000111", "Sjeverni logor", "Sjeverni logor", "Mostar", "", "88000", 1, "Drzava")]
+        [InlineData("Zorka", "Kunovac", "zorka.kunovac1@hotmail.com", "+38761000111", "Sjeverni logor", "Sjeverni logor", "Mostar", "Bosna i Hercegovina", "", 1, "PostanskiBroj")]
+        [InlineData("Zorka", "Kunovac", "zorka.kunovac1@hotmail.com", "+38761000111", "Sjeverni logor", "Sjeverni logor", "Mostar", "Bosna i Hercegovina", "88000", 0, "KorisnikId")]
+        public void AddKupac_EmptyField_ShouldFail(string ime, string prezime, string email, string brojTelefona, string adresa1, string adresa2, string grad, string drzava, string postanskiBroj, int korisnikId, string param)
+        {
+            KupacUpsertRequest request = new KupacUpsertRequest()
+            {
+                Ime = ime,
+                Prezime = prezime,
+                Email = email,
+                BrojTelefona = brojTelefona,
+                Adresa1 = adresa1,
+                Adresa2 = adresa2,
+                Grad = grad,
+                Drzava = drzava,
+                PostanskiBroj = postanskiBroj,
+                KorisnikId = korisnikId
+            };
+
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase(databaseName: "KupacContext2")
+            .Options;
+
+            using (_context = new ApplicationDbContext(options))
+            {
+                KupacService _service = new KupacService(_context, _mapper);
+                //assert & act
+                Assert.Throws<ArgumentException>(param, () => _service.Insert(request));
+            }
+        }
+
+        [Theory]
+        [InlineData("Ana-Marija", "Brlic-Mazuranic", "ana-marija@hotmail.com", "+38761000111", "Neka-Adresa", "Neka-Adresa", "Mostar", "Bosna i Hercegovina","SlovniPostanskiBroj",2)]
+
+        public void AddKupac_Format_ShouldWork(string ime, string prezime, string email, string brojTelefona, string adresa1, string adresa2, string grad, string drzava, string postanskiBroj, int korisnikId)
+        {
+            KupacUpsertRequest request = new KupacUpsertRequest()
+            {
+                Ime = ime,
+                Prezime = prezime,
+                Email = email,
+                BrojTelefona = brojTelefona,
+                Adresa1 = adresa1,
+                Adresa2 = adresa2,
+                Grad = grad,
+                Drzava = drzava,
+                PostanskiBroj = postanskiBroj,
+                KorisnikId = korisnikId
+            };
+
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase(databaseName: "KupacContext3")
+            .Options;
+
+            using (_context = new ApplicationDbContext(options))
+            {
+                KupacService _service = new KupacService(_context, _mapper);
+                //assert & act
+                var validKupac = _service.Insert(request);
+                Assert.NotNull(validKupac);
+            }
+        }
+
+        [Theory]
+        [InlineData("ParametarImePrekoDozvoljenogBrojaKaraktera1Br0j3v1", "Kunovac", "zorka.kunovac1@hotmail.com", "+38761000111", "Sjeverni logor", "Sjeverni logor", "Mostar", "Bosna i Hercegovina", "88000", 1, "Ime")]
+        [InlineData("Zorka", "ParametarPrezimePrekoDozvoljenogBrojaKarakteraBr0j3v1", "zorka.kunovac1@hotmail.com", "+38761000111", "Sjeverni logor", "Sjeverni logor", "Mostar", "Bosna i Hercegovina", "88000", 1, "Prezime")]
+        [InlineData("Zorka", "Kunovac", "example.com", "+38761000111", "Sjeverni logor", "Sjeverni logor", "Mostar", "Bosna i Hercegovina", "88000", 1, "Email")]
+        [InlineData("Zorka", "Kunovac", "zorka@example.com", "+3876100011231", "Sjeverni logor", "Sjeverni logor", "Mostar", "Bosna i Hercegovina", "88000", 1, "BrojTelefona")]
+        [InlineData("Zorka", "Kunovac", "zorka@example.com", "+38761000111", "New", "Sjeverni logor", "Mostar", "Bosna i Hercegovina", "88000", 1, "Adresa1")]
+        [InlineData("Zorka", "Kunovac", "zorka@example.com", "+38761000111", "Sjeverni logor", "Sjeverni logor", "New", "Bosna i Hercegovina", "88000", 1, "Grad")]
+
+        public void AddKupac_Format_ShouldFail(string ime, string prezime, string email, string brojTelefona, string adresa1, string adresa2, string grad, string drzava, string postanskiBroj, int korisnikId, string param)
+        {
+            KupacUpsertRequest request = new KupacUpsertRequest()
+            {
+                Ime = ime,
+                Prezime = prezime,
+                Email = email,
+                BrojTelefona = brojTelefona,
+                Adresa1 = adresa1,
+                Adresa2 = adresa2,
+                Grad = grad,
+                Drzava = drzava,
+                PostanskiBroj = postanskiBroj,
+                KorisnikId = korisnikId
+            };
+
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase(databaseName: "KupacContext4")
+            .Options;
+
+            using (_context = new ApplicationDbContext(options))
+            {
+                KupacService _service = new KupacService(_context, _mapper);
+                //assert & act
+                Assert.Throws<ArgumentException>(param, () => _service.Insert(request));
+            }
+        }
+
+    }
+}
